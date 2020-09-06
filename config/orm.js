75c5147b3150b47a1d56ca4eb1da28ad43824d7e
Aurora Brune <akbrune@hotmail.com>
2020-09-06 14:34:28 -0500

    main handlebars file

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,19 @@
 const connection = require('./connection.js');
 
+var orm = {
+    select: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+}
 //methods to retrieve and store data in burger_db
 //selectAll() selectOne() updateOn
-orm.select("")
+orm.selectAll("")
 
 
 
