17cd5176ff903ce21e1059840233703ea0e11aca
Aurora Brune <akbrune@hotmail.com>
2020-09-07 18:23:18 -0500

    burger.js api page

---
+++ public/assets/js/burger.js
@@ -0,0 +1,36 @@
+$(function () {
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#newBurger").val().trim(),
+            devoured: 0
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("Added new burger");
+                //reload the page to update the list
+                location.reload();
+            }
+        );
+    });
+    $(".eatburger").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).data("id");
+        var devouredState = {
+            devoured: 1
+        }
+
+        $.ajax("/api/burgers" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(function () {
+            console.log("Burger devoured");
+            location.reload();
+        })
+    });
+});
